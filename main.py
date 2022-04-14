ab6d7a97765ad223d7c4dd92142fae85e10e61de
niet841 <70171673+niet841@users.noreply.github.com>
2022-04-14 09:54:25 +0600

    Update main.py

--- main.py
+++ main.py
@@ -66,7 +66,7 @@ def webhook():
     bot.set_webhook(url='https://telbt.herokuapp.com/'+token)
     return'!',200
 if __name__=='__main__':
-    server.run(host='0.0.0.0',port=int(os.environ.get('PORT',8000)))
+    server.run(host='0.0.0.0',port=int(os.environ.get('PORT',5000)))
 
 
 
